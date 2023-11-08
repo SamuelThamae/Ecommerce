3d1bf1e884bfb1659730fcc67f93b11df456a737
Samuel Thamae <thamae.samuel@outlook.com>
2023-11-08 16:32:18 +0200

    create get category by id

--- routes/category.js
+++ routes/category.js
@@ -5,5 +5,6 @@ const auth=require('../Services/CheckToken')
 
 router.post('/',auth.tokenSend,auth.checkRole,category.create)
 .get('/',category.getAll)
+.get('/:id',auth.tokenSend,auth.checkRole,category.getOne)
 
 module.exports=router
